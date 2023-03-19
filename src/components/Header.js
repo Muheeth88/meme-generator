6170db62cafc2e11a8e060374df82516754af990
Abdul Muheeth <abdulmuheeth88@gmail.com>
2023-03-20 00:30:38 +0530

    font awesome added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { faFaceAngry } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <header className="flex justify-between bg-purple-600 py-4 text-white">
-      <span className="mx-4 text-3xl font-bold">Meme Generator</span>
+      <span className="mx-4 text-3xl font-bold">
+        <FontAwesomeIcon
+          className="mr-4 text-4xl font-extrabold"
+          icon={faFaceAngry}
+        />
+        Meme Generator
+      </span>
       <span className="mx-4 text-base mt-auto">-made with React</span>
     </header>
   );
